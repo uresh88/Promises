ecfe973772063190185cd9bfc862eeaa8423e512
uresh <uresh@embla.asia>
2016-03-09 12:33:41 +0530

    test

---
+++ MvcApplication1/MvcApplication1/Promise/promises-home.js
@@ -0,0 +1,16 @@
+var page = function () {
+    this.paraCount = 0
+}
+
+page.prototype.pullParagraph = function(url) {
+	return fetch(url);
+};
+
+var mainFunction = new function () {
+	var currentPage = new page();
+	currentPage.pullParagraph("https://raw.githubusercontent.com/uresh88/Promises/master/firstPara.json");
+
+}
+
+
+document.addEventListener('DOMContentLoaded', mainFunction, false);
